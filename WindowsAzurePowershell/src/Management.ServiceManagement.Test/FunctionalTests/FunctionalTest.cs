dd263dd443d4802a8fae1ae365fc4318383150c3
unknown <Hyonho.Lee@microsoft.com>
2013-09-27 14:18:03 -0700

    fix test

--- WindowsAzurePowershell/src/Management.ServiceManagement.Test/FunctionalTests/FunctionalTest.cs
+++ WindowsAzurePowershell/src/Management.ServiceManagement.Test/FunctionalTests/FunctionalTest.cs
@@ -296,7 +296,7 @@ namespace Microsoft.WindowsAzure.Management.ServiceManagement.Test.FunctionalTes
                 Console.WriteLine("Auto upgrade took {0}.", duration);
 
                 result = vmPowershellCmdlets.GetAzureDeployment(serviceName, DeploymentSlotType.Production);
-                pass &= Utilities.PrintAndCompareDeployment(result, serviceName, deploymentName, serviceName, DeploymentSlotType.Production, null, 3);
+                pass &= Utilities.PrintAndCompareDeployment(result, serviceName, deploymentName, serviceName, DeploymentSlotType.Production, null, 4);
                 Console.WriteLine("successfully updated the deployment");
                                
                 vmPowershellCmdlets.RemoveAzureDeployment(serviceName, DeploymentSlotType.Production, true);
